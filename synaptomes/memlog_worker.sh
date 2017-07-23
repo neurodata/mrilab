61346d0cf39de8502b3101788dff77ed7225047b
eric bridgeford <ebridge2@jhu.edu>
2017-07-22 22:36:27 -0400

    updated synaptomes

--- synaptomes/memlog_worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-# run this in the background with nohup ./memlog.sh > mem.txt &
-# written by eric bridgeford
-
-while true; do
-    echo "$(free -m | grep Mem | awk '{print $3}')"
-    sleep $1
-done
